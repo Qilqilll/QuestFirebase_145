8cbcb6e8fc7e7a36b3d24d36250d643fbc6bde54
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2026-01-02 21:52:39 +0700

    Objek navigasi untuk layar Detail

---
+++ app/src/main/java/com/example/firebase_145/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.firebase_145.view.route
+
+import com.example.firebase_145.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}

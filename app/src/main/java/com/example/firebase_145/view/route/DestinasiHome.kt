ff55fa0c857d0176694a90da5297960788dd95f2
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2026-01-02 21:52:12 +0700

    Objek navigasi untuk layar Home

---
+++ app/src/main/java/com/example/firebase_145/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.firebase_145.view.route
+
+import com.example.firebase_145.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

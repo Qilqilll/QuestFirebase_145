444252de395b0f62603ddd5fd2096cc34b12c7a7
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2026-01-08 17:08:01 +0700

    initializer tiap bagian viewmodel

--- app/src/main/java/com/example/firebase_145/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase_145/viewmodel/PenyediaViewModel.kt
@@ -1,19 +1,39 @@
 package com.example.firebase_145.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.firebase_145.repository.AplikasiDataSiswa
+import com.example.firebase_145.repository.ContainerApp
+import com.example.firebase_145.repository.RepositorySiswa
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa
-        )
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [AplikasiDataSiswa].
+ */
+fun CreationExtras.aplikasiDataSiswa(): com.example.firebase_145.AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as com.example.firebase_145.AplikasiDataSiswa)
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }

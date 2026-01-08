a71051823d130e1a96bc1f3eb3b78e882af2fe7c
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2026-01-08 17:17:41 +0700

    Dua cara hapus data

--- app/src/main/java/com/example/firebase_145/repository/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase_145/repository/RepositorySiswa.kt
@@ -73,3 +73,14 @@ class FirebaseRepositorySiswa : RepositorySiswa {
             )
         ).await()
     }
+
+    override suspend fun hapusSatuSiswa(id: Long) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).delete().await()
+    }
+
+    override suspend fun deleteSiswa(siswa: Siswa) {
+        hapusSatuSiswa(siswa.id)
+    }
+}
